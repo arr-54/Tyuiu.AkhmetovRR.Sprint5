0744cb93b20fdb8f79deab0a5824c39e349fba0a
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-11-09 21:06:07 +0500

    Task0

--- Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib
         {
             string[] paths = { Path.GetTempPath(), Path.GetTempFileName() };
             string path = Path.Combine(paths);
-            double res = Math.Round(x / (Math.Pow(x, 3) + 2), 3);
+            double res = Math.Round((4-Math.Pow(x,3))/Math.Pow(x,2), 3);
             File.WriteAllText(path, Convert.ToString(res));
             return path;
         }
