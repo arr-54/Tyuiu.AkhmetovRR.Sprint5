e08b218ea6a9e7688a9a6c20801a9be7090ec3c0
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-11-09 21:04:53 +0500

    Task0

--- Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib/DataService.cs
@@ -6,9 +6,10 @@ namespace Tyuiu.FabritsiusAO.Sprint5.Task0.V11.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double y = Math.Round((4 - Math.Pow(x, 3)) / Math.Pow(x, 2), 3);
-            File.WriteAllText(path,Convert.ToString(y));
+            string[] paths = { Path.GetTempPath(), Path.GetTempFileName() };
+            string path = Path.Combine(paths);
+            double res = Math.Round(x / (Math.Pow(x, 3) + 2), 3);
+            File.WriteAllText(path, Convert.ToString(res));
             return path;
         }
     }
